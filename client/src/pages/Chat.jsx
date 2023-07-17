5aae4fc19f196934508e7e5ae13381561631c911
ShubhamR2407 <103954592+ShubhamR2407@users.noreply.github.com>
2023-07-17 16:58:29 +0530

    Update Chat.jsx

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -38,7 +38,7 @@ const Chat = () => {
   }, []);
 
   const connectToWS = () => {
-    const ws = new WebSocket("wss://" + window.location.host)
+    const ws = new WebSocket("wss://" + window.location.host:5000)
     setWs(ws);
     ws.addEventListener("message", handleMessage);
     ws.addEventListener("close", () => {
