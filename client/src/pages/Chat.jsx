d9fc63a0953bc4a9f1abf97987ecf4ba03bc1c3f
ShubhamR2407 <103954592+ShubhamR2407@users.noreply.github.com>
2023-07-17 12:57:45 +0530

    Update Chat.jsx

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -38,7 +38,7 @@ const Chat = () => {
   }, []);
 
   const connectToWS = () => {
-    const ws = new WebSocket("ws://localhost:5000");
+    const ws = new WebSocket("ws://just-chat-server-ebon.vercel.app/api");
     setWs(ws);
     ws.addEventListener("message", handleMessage);
     ws.addEventListener("close", () => {
